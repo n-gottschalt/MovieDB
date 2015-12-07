06803d7b588dec0d4b080eeb9390ff25250538b3
Nathaniel Gottschalt <n.gottschalt@gmail.com>
2015-12-07 16:04:53 -0500

    Added Database test class.

---
+++ src/databaseJSON/DatabaseTest.java
@@ -0,0 +1,29 @@
+package databaseJSON;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DatabaseTest {
+
+	@Test
+	public void testConnection() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetData() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDataString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMain() {
+		fail("Not yet implemented");
+	}
+
+}
